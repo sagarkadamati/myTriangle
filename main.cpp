af050531bcac03cc1122f8a5e41526f6a49407b4
Sagar Kadamati <sagarkadamati@gmail.com>
2016-07-24 19:20:43 +0530

    Shaders: Added Shader class

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "display.h"
+#include "shader.h"
 
 int main(int argc, char* argv[])
 {
 	Display display(1280, 720, "Hello World!");
 
+	Shader shader("./res/basicShader");
+
 	while(!display.IsClosed())
 	{
 		display.Clear(1.0f, 1.0f, 1.0f, 1.0f);
+
+		shader.Bind();
 
 		display.Update();
 	}
